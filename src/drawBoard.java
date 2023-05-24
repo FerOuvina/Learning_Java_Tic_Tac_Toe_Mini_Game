bec6edd4fc8fbe8889b16d7c221648964f91027c
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-05-24 18:40:30 -0300

    Creating the draw board method and extracting it into a separate file

--- src/drawBoard.java
+++ src/drawBoard.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class drawBoard {
+public class drawBoard {
+  public drawBoard (String[] board) {
+	System.out.println("Drawing board...");
+	System.out.print(board[0] + " ");
+	System.out.print(board[1] + " ");
+	System.out.print(board[2] + "\n");
+	System.out.print(board[3] + " ");
+	System.out.print(board[4] + " ");
+	System.out.print(board[5] + "\n");
+	System.out.print(board[6] + " ");
+	System.out.print(board[7] + " ");
+	System.out.print(board[8] + "\n");
+  }
 }
