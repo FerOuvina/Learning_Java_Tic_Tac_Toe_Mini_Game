8709aeeb4d61835adebb48113ae00e7c45ebee6b
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-05-24 18:41:33 -0300

    Setting up the game

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+  public static void main (String[] args) {
+	String[] board = new String[9];
+	Scanner scanner = new Scanner(System.in);
+	String turn = "x";
+	Boolean winner = false;
+	for (int i = 0; i < board.length; i++) {
+	  board[i] = String.valueOf(i + 1);
+	}
+
+	new drawBoard(board);
+
+	System.out.println("\n" + "Starting game...");
+	System.out.println("Is the turn of player: " + turn);
+	System.out.println("Please select the number where you like to place your mark...");
+	int userInput = scanner.nextInt();
+	board[userInput - 1] = turn;
+
+	new drawBoard(board);
+  }
+
+
 }
+
