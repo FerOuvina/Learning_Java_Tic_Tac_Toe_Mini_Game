72105f37cc1ac6c2fe65843798505256bdca4940
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-05-24 18:40:09 -0300

    Creating the check winner method and extracting it into a separate file

--- src/checkWinner.java
+++ src/checkWinner.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class checkWinner {
+public class checkWinner {
+  public checkWinner (String[] board, Boolean winner, String turn) {
+	if (board[0].equals("x") && board[1].equals("x") && board[2].equals("x")) {
+	  winner = true;
+	  System.out.println("Game over, the winner is the player: " + turn);
+	} else if (board[3].equals("x") && board[4].equals("x") && board[5].equals("x")) {
+	  winner = true;
+	  System.out.println("Game over, the winner is the player: " + turn);
+	} else if (board[6].equals("x") && board[7].equals("x") && board[8].equals("x")) {
+	  winner = true;
+	  System.out.println("Game over, the winner is the player: " + turn);
+	} else if (board[0].equals("x") && board[3].equals("x") && board[6].equals("x")) {
+	  winner = true;
+	  System.out.println("Game over, the winner is the player: " + turn);
+	} else if (board[1].equals("x") && board[4].equals("x") && board[7].equals("x")) {
+	  winner = true;
+	  System.out.println("Game over, the winner is the player: " + turn);
+	} else if (board[2].equals("x") && board[5].equals("x") && board[8].equals("x")) {
+	  winner = true;
+	  System.out.println("Game over, the winner is the player: " + turn);
+	} else if (board[0].equals("x") && board[4].equals("x") && board[8].equals("x")) {
+	  winner = true;
+	  System.out.println("Game over, the winner is the player: " + turn);
+	} else if (board[2].equals("x") && board[4].equals("x") && board[6].equals("x")) {
+	  winner = true;
+	  System.out.println("Game over, the winner is the player: " + turn);
+	}
+  }
 }
